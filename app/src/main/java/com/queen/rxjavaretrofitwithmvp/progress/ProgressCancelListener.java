537bd159f7b5817a0598abfa557a448c7d4d6d76
oo1993448102 <19931123xue@163.com>
2016-08-03 11:54:22 +0800

    change whole application name

--- app/src/main/java/com/queen/rxjavaretrofitwithmvp/progress/ProgressCancelListener.java
+++
@@ -1,8 +0,0 @@
-package com.queen.rxjavaretrofitwithmvp.progress;
-
-/**
- * Created by liukun on 16/3/10.
- */
-public interface ProgressCancelListener {
-    void onCancelProgress();
-}
--- app/src/main/java/com/queen/rxjavaretrofitwithmvp/subscribers/SubscriberOnNextListener.java
+++
@@ -1,8 +0,0 @@
-package com.queen.rxjavaretrofitwithmvp.subscribers;
-
-/**
- * Created by liukun on 16/3/10.
- */
-public interface SubscriberOnNextListener<T> {
-    void onNext(T t);
-}
